97c0d493fb568efb95db676af6c45198dcefff17
Claudio Mldo <clam0395@gmail.com>
2022-11-17 23:18:29 -0500

    Carga masiva cobus y eades

--- app/Http/Controllers/EadeController.php
+++ app/Http/Controllers/EadeController.php
@@ -20,65 +20,91 @@ class EadeController extends Controller
 
     public function list()
     {
-        $list = Eade::orderBy('anio_modelo', 'desc')->orderBy('marca','desc')->get();
+        $list = Eade::orderBy('anio_modelo', 'desc')->orderBy('marca', 'desc')->get();
         return $this->getOk($list);
     }
 
     public function searchProductsEade(Request $request)
     {
         $input = $request->all();
-        $marca=$request['marca'];
-        $modelo=strtoupper($request['modelo']);
+        $marca = $request['marca'];
+        $modelo = strtoupper($request['modelo']);
 
-         if ($request['marca']=='null' || $request['marca']==''){
-            $marca='0';
-        }else{
-            $marca=$request['marca'];
+        if ($request['marca'] == 'null' || $request['marca'] == '') {
+            $marca = '0';
+        } else {
+            $marca = $request['marca'];
         }
 
-        if ($request['modelo']=='null' || $request['modelo']==''){
-            $modelo='0';
-        }else{
-            $modelo=$request['modelo'];
+        if ($request['modelo'] == 'null' || $request['modelo'] == '') {
+            $modelo = '0';
+        } else {
+            $modelo = $request['modelo'];
         }
 
-       try{
-            $sql="select distinct codigo, marca,modelo,segmento
+        try {
+            $sql = "select distinct codigo, marca,modelo,segmento
                 from eade
-                where if ('".$marca."'='0',true,marca like '%".$marca."%') and
-                        if ('".$modelo."'='0',true,modelo like '%".$modelo."%')";
+                where if ('" . $marca . "'='0',true,marca like '%" . $marca . "%') and
+                        if ('" . $modelo . "'='0',true,modelo like '%" . $modelo . "%')";
 
             $list = DB::select($sql);
             return $this->getOk($list);
-
-        }catch(\Exception $e) {
+        } catch (\Exception $e) {
             return $this->insertErrCustom($request, $e->getMessage());
         }
     }
 
     public function create(Request $request)
     {
-        try{
+        try {
             $input = $request->all();
             //DB::beginTransaction();
             //grabamos el detalle
             foreach ($input['detalle'] as $detalle) {
-                $entidad = new Eade($detalle);
-                $entidad->save();
+
+                $sql = "select count(id) from eade WHERE
+                (codigo=?) and
+                (anio=?) and
+                (mes=?) and
+                (anio_modelo=?) and
+                (marca=?) and
+                (modelo=?) and
+                (segmento=?) and
+                (provincia=?) and
+                (canton=?) and
+                (avaluo=?) and
+                (unidades=?);";
+
+                $count = DB::select(
+                    $sql,
+                    [
+                        $detalle['codigo'],
+                        $detalle['anio'],
+                        $detalle['mes'],
+                        $detalle['anio_modelo'],
+                        $detalle['marca'],
+                        $detalle['modelo'],
+                        $detalle['segmento'],
+                        $detalle['provincia'],
+                        $detalle['canton'],
+                        $detalle['avaluo'],
+                        $detalle['unidades']
+                    ]
+                );
+
+                #return $this->insertOk($count);
+
+                if ($count[0]->count <= 0) {
+                    $entidad = new Eade($detalle);
+                    $entidad->save();
+                }
             };
             //  DB::commit();
             return $this->insertOk($input);
-
         } catch (\Exception $e) {
             //DB::rollBack();
             return $this->insertErrCustom(null, $e->getMessage());
         }
-
     }
-
-
-
-
-
-
 }
