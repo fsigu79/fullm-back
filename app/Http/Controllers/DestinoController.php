937c09359dff64940f108216f080159bd413f148
fsigu79 <71946416+fsigu79@users.noreply.github.com>
2023-05-16 16:08:57 -0500

    fsigu siniestros

--- app/Http/Controllers/DestinoController.php
+++ app/Http/Controllers/DestinoController.php
@@ -18,9 +18,23 @@ class DestinoController extends Controller{
         $this->middleware('auth:admin');
     }
 
-     public function listNumeros()
+    public function listNumeros()
     {
         $doc='IN';
+
+        $sql=  "select d.id,d.nombre,d.serie,doc.numero,doc.codigo_contable
+                from destinos d
+                inner join documentos doc on d.serie=doc.serie and doc.codigo=? and doc.modulo='Inventarios'
+                where d.esactivo=1";
+
+        $list = DB::select($sql,[$doc]);
+
+        return $this->getOk($list);
+    }
+
+    public function listNumerosSiniestros()
+    {
+        $doc='SI';
 
         $sql=  "select d.id,d.nombre,d.serie,doc.numero,doc.codigo_contable
                 from destinos d
